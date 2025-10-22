533ea1b6c695be5adfe262e1faf86e51aec9f8e6
Avery Williams <awilliams@chapman.edu>
2025-10-22 16:47:10 +0000

    y

--- username.sh
+++ username.sh
@@ -8,12 +8,13 @@ echo "It has to be at least 3 characters but no more than 12"
 echo "It has to start with a lowercase letter"
 echo "The rest can be lowercase, uppercase, numbers, or an underscore"
 
-while IFS= read -r line; do
-    read -p "Type in a username: " user_name 
-    if echo "$user_name" | grep -E -v '^[a-z][a-zA-Z0-9_]{2,11}$' > /dev/null 2>&1; then
-        echo "Try again"
-    else
+while true; do
+    read -p "Type in a username: " user_name
+    if [[ $user_name =~ ^[a-z][a-zA-Z0-9_]{2,11}$ ]]; then
         echo "Your username is '$user_name'"
+        break
+    else
+        echo "Try again"
     fi
 done
 echo "Thank you!"
